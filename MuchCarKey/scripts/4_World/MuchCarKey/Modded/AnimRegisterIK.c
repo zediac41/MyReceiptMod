38737dd70fb6bc9628d7cbac0c367e46d0b4ca3d
zediac41 <iamthe_shiznit@yahoo.com>
2025-10-20 14:45:28 -0500

    Add IK profiles for car key items

---
+++ MuchCarKey/scripts/4_World/MuchCarKey/Modded/AnimRegisterIK.c
@@ -0,0 +1,11 @@
+modded class ModItemRegisterCallbacks
+{
+    override void RegisterOneHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
+    {
+        super.RegisterOneHanded(pType, pBehavior);
+        pType.AddItemInHandsProfileIK("MCK_CarKey_Base", "dz/anims/workspaces/player/player_main/props/player_main_1h_keys.asi", pBehavior, "dz/anims/anm/player/ik/gear/handcuff_keys.anm");
+        pType.AddItemInHandsProfileIK("MCK_CarKeyTag_Base", "dz/anims/workspaces/player/player_main/props/player_main_1h_keys.asi", pBehavior, "dz/anims/anm/player/ik/gear/handcuff_keys.anm");
+        pType.AddItemInHandsProfileIK("MCK_MasterKey", "dz/anims/workspaces/player/player_main/props/player_main_1h_keys.asi", pBehavior, "dz/anims/anm/player/ik/gear/handcuff_keys.anm");
+        pType.AddItemInHandsProfileIK("MCK_KeyPouch", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/paper.anm");
+    };
+};
